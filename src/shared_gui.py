70efd81182bd7265f4bf9b8079532d35f479976a
ozatalcs <ozatalcs@rose-hulman.edu>
2019-02-07 12:33:37 -0500

    made gui

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -259,7 +259,7 @@ def handle_quit(mqtt_sender):
     Tell the robot's program to stop its loop (and hence quit).
       :type  mqtt_sender:  com.MqttClient
     """
-
+    mqtt_sender.send_message("stop")
 
 def handle_exit(mqtt_sender):
     """
@@ -267,3 +267,5 @@ def handle_exit(mqtt_sender):
     Then exit this program.
       :type mqtt_sender: com.MqttClient
     """
+    mqtt_sender.send_message("stop")
+
