18ac2eb543c6417e1e1853f4172227383b509827
JrOtaku <emilya200.ea@gmail.com>
2019-02-07 12:30:32 -0500

    I wrote delegates for the rosebot methods on shared_gui_delegrate_on_robot

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -14,3 +14,28 @@ class DelagateThatReceives(object):
 
     def forward(self, left_wheel_speed, right_wheel_speed):
         self.robot.drive_system.go(int(left_wheel_speed), int(right_wheel_speed))
+
+    def backwards(self, left_wheel_speed, right_wheel_speed):
+        self.robot.drive_system.go(-int(left_wheel_speed), -int(right_wheel_speed))
+
+    def left(self, left_wheel_speed, right_wheel_speed):
+        self.robot.drive_system.go(-int(left_wheel_speed), int(right_wheel_speed))
+
+    def right(self, left_wheel_speed, right_wheel_speed):
+        self.robot.drive_system.go(int(left_wheel_speed), -int(right_wheel_speed))
+
+    def stop(self):
+        self.robot.drive_system.stop()
+
+    def raise_arm(self):
+        self.robot.arm_and_claw.raise_arm()
+
+    def lower_arm(self):
+        self.robot.arm_and_claw.lower_arm()
+
+    def calibrate_arm(self):
+        self.robot.arm_and_claw.calibrate_arm()
+
+    def move_arm_to_position(self, arm_position_entry):
+        self.robot.arm_and_claw.move_arm_to_position(arm_position_entry)
+
