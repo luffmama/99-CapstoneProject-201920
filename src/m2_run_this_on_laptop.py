fffd833fa3b24317fb8659dd088b577abc9b43b5
JrOtaku <emilya200.ea@gmail.com>
2019-02-09 19:58:07 -0500

    I added stuff to my m2_run_on_this_laptop

--- src/m2_run_this_on_laptop.py
+++ src/m2_run_this_on_laptop.py
@@ -41,7 +41,7 @@ def main():
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
     teleop_frame, arm_frame, control_frame = get_shared_frames(main_frame, mqtt_sender)
-
+    movement_frame = get_movement_frame(main_frame, mqtt_sender)
     # -------------------------------------------------------------------------
     # Frames that are particular to my individual contributions to the project.
     # -------------------------------------------------------------------------
@@ -50,7 +50,7 @@ def main():
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
-    grid_frames(teleop_frame, arm_frame, control_frame)
+    grid_frames(teleop_frame, arm_frame, control_frame, movement_frame)
 
     # -------------------------------------------------------------------------
     # The event loop:
@@ -65,11 +65,73 @@ def get_shared_frames(main_frame, mqtt_sender):
     return teleop, arm, control
 
 
-def grid_frames(teleop_frame, arm_frame, control_frame):
+def grid_frames(teleop_frame, arm_frame, control_frame, movement_frame):
     teleop_frame.grid(row=0, column=0)
     arm_frame.grid(row=1, column=0)
     control_frame.grid(row=2, column=0)
+    movement_frame.grid(row=0, column=1)
 
+
+
+def get_movement_frame(window, mqtt_sender):
+    """
+    Constructs and returns a frame on the given window, where the frame
+    has Entry and Button objects that control the EV3 robot's motion
+    by passing messages using the given MQTT Sender.
+      :type  window:       ttk.Frame | ttk.Toplevel
+      :type  mqtt_sender:  com.MqttClient
+    """
+
+    # Construct the frame to return:
+    frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
+    frame.grid()
+
+    # Construct the widgets on the frame:
+    frame_label = ttk.Label(frame, text="Movement")
+    straight_for_seconds_label = ttk.Label(frame, text="Seconds for Robot to move")
+    straight_for_inches_using_seconds_label = ttk.Label(frame, text="Inches for Robot to move (using seconds)")
+    straight_for_inches_using_encoder_label = ttk.Label(frame, text="Inches for Robot to move (using sensors)")
+
+    # Creates input boxes
+    straight_for_seconds_entry = ttk.Entry(frame, width=8)
+    straight_for_inches_using_seconds_entry = ttk.Entry(frame, width=8)
+    straight_for_inches_using_encoder_entry = ttk.Entry(frame, width=8)
+    # left_speed_entry.insert(0, "100")
+    # right_speed_entry.insert(0, "100")
+    # The two commented out lines insert a initial value into the input box.
+
+    straight_for_seconds_button = ttk.Button(frame, text="Go!")
+    straight_for_inches_using_seconds_button = ttk.Button(frame, text="Go!")
+    straight_for_inches_using_encoder_button = ttk.Button(frame, text="Go!")
+
+    # Grid the widgets:
+    frame_label.grid(row=1, column=0)
+    straight_for_seconds_label.grid(row=1, column=0)
+    straight_for_seconds_entry.grid(row=2, column=0)
+    straight_for_seconds_button.grid(row=3, column=0)
+
+    straight_for_inches_using_seconds_label.grid(row=1, column=1)
+    straight_for_inches_using_seconds_entry.grid(row=2, column=1)
+    straight_for_inches_using_seconds_button.grid(row=3, column=1)
+
+    straight_for_inches_using_encoder_label.grid(row=1, column=2)
+    straight_for_inches_using_encoder_entry.grid(row=2, column=2)
+    straight_for_inches_using_encoder_button.grid(row=3, column=2)
+
+    """
+    # Set the button callbacks:
+    forward_button["command"] = lambda: handle_forward(
+        left_speed_entry, right_speed_entry, mqtt_sender)
+    backward_button["command"] = lambda: handle_backward(
+        left_speed_entry, right_speed_entry, mqtt_sender)
+    left_button["command"] = lambda: handle_left(
+        left_speed_entry, right_speed_entry, mqtt_sender)
+    right_button["command"] = lambda: handle_right(
+        left_speed_entry, right_speed_entry, mqtt_sender)
+    stop_button["command"] = lambda: handle_stop(mqtt_sender)
+    """
+
+    return frame
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
