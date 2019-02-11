170d93106ad5f30926ac6df9bdfb0386c65d3118
ozatalcs <ozatalcs@rose-hulman.edu>
2019-02-11 12:04:47 -0500

    wrote test for tone sequence and beeper

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -388,7 +388,8 @@ def handle_quit(mqtt_sender):
     Tell the robot's program to stop its loop (and hence quit).
       :type  mqtt_sender:  com.MqttClient
     """
-    #mqtt_sender.send_message("quit")
+    print('quit')
+    mqtt_sender.send_message('quit')
 
 def handle_exit(mqtt_sender):
     """
@@ -396,8 +397,9 @@ def handle_exit(mqtt_sender):
     Then exit this program.
       :type mqtt_sender: com.MqttClient
     """
-    #handle_quit(mqtt_sender)
-    #exit()
+    print('exit')
+    handle_quit(mqtt_sender)
+    exit()
 
 ################################################################################
 # Handlers for Buttons in Movement frame
