630dbfd1c23d12415a0c7d72e979d8b249991d6d
ozatalcs <ozatalcs@rose-hulman.edu>
2019-02-13 08:12:08 -0500

    my gui for sprint 2

--- src/m3_run_this_on_laptop.py
+++ src/m3_run_this_on_laptop.py
@@ -80,17 +80,18 @@ def pick_up_object_while_beeping_frame(main_frame, mqtt_sender):
     pick_up_with_beeps_frame.grid()
 
     # initial beep speed
-    initial_beep_speed_label = ttk.Label(pick_up_with_beeps_frame, text='enter initial beeps per second')
+    initial_beep_speed_label = ttk.Label(pick_up_with_beeps_frame, text='enter initial beeps per second:')
     initial_beep_speed_label.grid(row=0, column=0)
     initial_beep_speed_entry = ttk.Entry(pick_up_with_beeps_frame, width=8)
     initial_beep_speed_entry.grid(row=0, column=1)
 
     # beep acceleration
-    beep_acceleration_label = ttk.Label(pick_up_with_beeps_frame, text='enter beep acceleration')
+    beep_acceleration_label = ttk.Label(pick_up_with_beeps_frame, text='enter beep acceleration:')
     beep_acceleration_label.grid(row=1, column=0)
     beep_acceleration_entry = ttk.Entry(pick_up_with_beeps_frame, width=8)
     beep_acceleration_entry.grid(row=1, column=1)
 
+    # button
     initial_beep_speed_button = ttk.Button(pick_up_with_beeps_frame, text='GO!')
     initial_beep_speed_button.grid(row=2, column=0)
 
@@ -98,19 +99,30 @@ def pick_up_object_while_beeping_frame(main_frame, mqtt_sender):
 
     return pick_up_with_beeps_frame
 
-def spin_untill_see_object_frame(main_frame, mqtt_sender):
+def spin_until_see_object_frame(main_frame, mqtt_sender):
     spin_till_see_object_frame = ttk.Frame(main_frame, padding=10, borderwidth=5, relief='groove')
     spin_till_see_object_frame.grid()
 
     # turn direction
-    direction_label = ttk.Label(spin_till_see_object_frame, text='clockwise spin(cw) or counterclockwise spin(ccw')
-    direction_label.grid()
+    direction_label = ttk.Label(spin_till_see_object_frame, text='clockwise spin(cw) or counterclockwise spin(ccw):')
+    direction_label.grid(row=0, column=0)
     direction_entry = ttk.Entry(spin_till_see_object_frame, width=8)
-    direction_entry.grid()
+    direction_entry.grid(row=0, column=1)
 
     # spin speed
-    spin_speed_label = ttk.Label(spin_till_see_object_frame)
-    
+    spin_speed_label = ttk.Label(spin_till_see_object_frame, text='enter spin speed:')
+    spin_speed_label.grid(row=1, column=0)
+    spin_speed_entry = ttk.Entry(spin_till_see_object_frame, width=8)
+    spin_speed_entry.grid(row=1, column=1)
+
+    # button
+    spin_till_see_object_button = ttk.Button(spin_till_see_object_frame, text='GO!')
+    spin_till_see_object_button.grid(row=2, column=0)
+
+    spin_till_see_object_button['command'] = lambda: print(direction_entry, spin_speed_entry)
+
+    return spin_till_see_object_frame
+
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
