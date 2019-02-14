264054ab7b9c1b882b837c748b4ff0258575c4bf
ozatalcs <ozatalcs@rose-hulman.edu>
2019-02-13 19:31:20 -0500

    shared gui feature 9.1

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -148,11 +148,12 @@ class DelagateThatReceives(object):
     def pick_up_object_while_beeping(self, initial_beep_speed_entry, beep_acceleration_entry):
         self.robot.drive_system.go(75, 75)
         while True:
-            if self.robot.sensor_system.ir_proximity_sensor.get_distance() <= 5:
+            if self.robot.sensor_system.ir_proximity_sensor.get_distance() <= 3:
+                self.robot.drive_system.stop()
                 break
             self.robot.sound_system.beeper.beep().wait()
             time.sleep(1/int(initial_beep_speed_entry) -
-                       (int(beep_acceleration_entry)*5)/((int(initial_beep_speed_entry))*
+                       (int(beep_acceleration_entry)*3)/((int(initial_beep_speed_entry))*
                         self.robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()))
         self.robot.arm_and_claw.raise_arm()
 
