c69cbc0486116a000af61d659836c5ae18b3eebf
Robert L. Kreft <kreftrl@rose-hulman.edu>
2019-02-15 18:36:59 -0500

    working on my m4_extra

--- src/m4_extra.py
+++ src/m4_extra.py
@@ -1,3 +1,5 @@
+#robert kreft's code
+
 import rosebot
 import mqtt_remote_method_calls as com
 import time
@@ -12,52 +14,84 @@ class m4_extra(object):
         self.robot = robot
         self.is_quit = False
 
-    #bang bang method of line following
-    def bang_bang_straight_line_follow(self,speed):
-        intensity=10
-        self.robot.drive_system.go(speed,speed)
-        #assume on right side of line when gets off
-        while True: #first loop
-            if self.robot.sensor_system.color_sensor.get_reflected_light_intensity()>intensity: #if robot leaves black line:
-                self.robot.drive_system.go(.75*speed,speed) #turns slightly to the left
-                time.sleep(.25) #after some time
-                self.robot.drive_system.go(speed,speed) #goes straight
-                time_set=time.clock() #timer check
-                while True: #second loop
-                    if self.robot.sensor_system.color_sensor.get_reflected_light_intensity()<intensity: #if it gets back on the black line
-                        self.robot.drive_system.go(speed,-speed) #spins right
-                        time.sleep(.1)
-                        break #break second loop
-                    if time_set-time.time()>=1: #if time exceeds 1 second, must be on left side of line
-                        self.robot.drive_system(speed,.75*speed) #turn right
-                        time.sleep(.5)
-                        self.robot.drive_system.go(speed,speed)
-                        while True: #third loop
-                            if self.robot.sensor_system.color_sensor.get_reflected_light_intensity()<intensity: #if it gets back on the black line
-                                self.robot.drive_system(-speed,speed) #spins left
-                                time.sleep(.1)
-                                break #break third loop
-                        break #break second loop
-            if self.robot.sensor_system.touch_sensor.is_pressed():
-                self.robot.drive_system.stop()
-                break #break first loop
-
-    #counts number of times an object is seen by the camera
-    def count_number_of_time_object_is_seen(self,count):
-        #if something is seen
-            #count=count+1
-        return count
+    # #bang bang method of line following
+    # def bang_bang_straight_line_follow(self,speed):
+    #     intensity=10
+    #     self.robot.drive_system.go(speed,speed)
+    #     #assume on right side of line when gets off
+    #     while True: #first loop
+    #         if self.robot.sensor_system.color_sensor.get_reflected_light_intensity()>intensity: #if robot leaves black line:
+    #             self.robot.drive_system.go(.75*speed,speed) #turns slightly to the left
+    #             time.sleep(.25) #after some time
+    #             self.robot.drive_system.go(speed,speed) #goes straight
+    #             time_set=time.clock() #timer check
+    #             while True: #second loop
+    #                 if self.robot.sensor_system.color_sensor.get_reflected_light_intensity()<intensity: #if it gets back on the black line
+    #                     self.robot.drive_system.go(speed,-speed) #spins right
+    #                     time.sleep(.1)
+    #                     break #break second loop
+    #                 if time_set-time.time()>=1: #if time exceeds 1 second, must be on left side of line
+    #                     self.robot.drive_system(speed,.75*speed) #turn right
+    #                     time.sleep(.5)
+    #                     self.robot.drive_system.go(speed,speed)
+    #                     while True: #third loop
+    #                         if self.robot.sensor_system.color_sensor.get_reflected_light_intensity()<intensity: #if it gets back on the black line
+    #                             self.robot.drive_system(-speed,speed) #spins left
+    #                             time.sleep(.1)
+    #                             break #break third loop
+    #                     break #break second loop
+    #         if self.robot.sensor_system.touch_sensor.is_pressed():
+    #             self.robot.drive_system.stop()
+    #             break #break first loop
+    #
+    # #counts number of times an object is seen by the camera
+    # def count_number_of_time_object_is_seen(self,count):
+    #     #if something is seen
+    #         #count=count+1
+    #     return count
 
     #bang bang method of line following circle in the clockwise direction
     def bang_bang_circ_line_follow_cw(self,speed):
         count=0
         self.robot.drive_system.go(speed,speed)
         while True:
+            if self.robot.sensor_system.touch_sensor.is_pressed():
+                self.robot.drive_system.stop()
+                break
             if self.robot.sensor_system.color_sensor.get_reflected_light_intensity()>10: #if robot gets off the line
-                self.robot.drive_system.go(-speed,speed)
+                self.robot.drive_system.go(speed,-speed)
                 while True:
+                    if self.robot.sensor_system.touch_sensor.is_pressed():
+                        self.robot.drive_system.stop()
+                        break
                     if self.robot.sensor_system.color_sensor.get_reflected_light_intensity() < 10:
                         self.robot.drive_system.go(speed,speed)
                         break
 
+    # bang bang method of line following circle in the counter clockwise direction
+    def bang_bang_circ_line_follow_ccw(self,speed):
+        count=0
+        self.robot.drive_system.go(speed,speed)
+        while True:
+            if self.robot.sensor_system.touch_sensor.is_pressed():
+                self.robot.drive_system.stop()
+                break
+            if self.robot.sensor_system.color_sensor.get_reflected_light_intensity()>10: #if robot gets off the line
+                self.robot.drive_system.go(-speed,speed)
+                while True:
+                    if self.robot.sensor_system.touch_sensor.is_pressed():
+                        self.robot.drive_system.stop()
+                        break
+                    if self.robot.sensor_system.color_sensor.get_reflected_light_intensity() < 10:
+                        self.robot.drive_system.go(speed,speed)
+                        break
 
+    def bang_bang_straight_line_follow_sor(self,speed):
+        self.robot.drive_system.go(speed,speed)
+        while True:
+            if self.robot.sensor_system.color_sensor.get_reflected_light_intensity() > 10:  # if robot gets off the line
+                self.robot.drive_system.go(speed,-speed)
+                while True:
+                    if self.robot.sensor_system.color_sensor.get_reflected_light_intensity() < 10:
+                        self.robot.drive_system.go(speed,speed)
+                        break
