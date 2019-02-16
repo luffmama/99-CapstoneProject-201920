c2f77b256b4806cf36bca798f4ea4fed74437820
ozatalcs <ozatalcs@rose-hulman.edu>
2019-02-16 15:49:55 -0500

    Working on sprint 3
    changed robot number to 20

--- src/m3_extra.py
+++ src/m3_extra.py
@@ -12,6 +12,7 @@ def m3_marlin_deep_sea(robot):
     print('Marlin deep sea activated')
     robot.drive_system.go(50, 50)
     while True:
+        print(robot.sensor_system.color_sensor.get_reflected_light_intensity)
         if robot.sensor_system.color_sensor.get_reflected_light_intensity() <= 10:
             robot.drive_system.stop()
             break
