802d43cd18a65674b3513a08c72e2845e8ddf911
ozatalcs <ozatalcs@rose-hulman.edu>
2019-02-16 12:43:29 -0500

    Working on sprint 3

--- src/m3_extra.py
+++ src/m3_extra.py
@@ -4,10 +4,8 @@
     Authors:  Conner Ozatalar.
     Winter term, 2018-2019.
 """
-
-def marlin_deap_sea():
+def m3_marlin_deap_sea():
     pass
 
-
-def nemo_deap_sea():
+def m3_nemo_deap_sea():
     pass
